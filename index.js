d6af843ec21c0ca99a5f5f0b6127393037679abb
Jackson Ho <mintri1199@gmail.com>
2019-06-04 12:49:13 -0700

    [Modified] added footer and pop up

--- index.js
+++ index.js
@@ -106,3 +106,36 @@ for (let i = 0, j = acc.length; i < j; i++) {
     }
   });
 }
+
+/*************
+Pop up section
+**************/
+
+// Declare default vars
+const popUp = document.getElementById('pop-up');
+const popUpLayer = document.getElementById('pop-up-layer');
+const closeModal = document.getElementById('close-pop-up');
+
+// nav-bar button 
+
+const subBtn = document.getElementById('sub-btn');
+
+//navbar button click makes the popup appear
+subBtn.addEventListener("click", () => {
+    popUpLayer.style.display = 'flex';
+})
+  
+  //close model click makes the popup disappear
+closeModal.addEventListener("click", function() {
+    popUpLayer.style.display = 'none';
+})
+
+let popUpSecond = 40;
+
+// trigger popup after seconds variable
+let popUpFun = window.setInterval(function() {
+    popUpLayer.style.display = 'flex';
+  
+    window.clearInterval(popUpFun);
+  
+}, popUpSecond * 1000);
